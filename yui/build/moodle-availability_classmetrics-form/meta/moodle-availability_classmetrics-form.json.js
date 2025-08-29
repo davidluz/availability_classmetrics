cbea650fcd2a9173a27c1bbb73f606b1162c72fa
davidluz <davidmelo8@hotmail.com>
2025-08-28 22:40:03 -0300

    Versão estável

--- yui/build/moodle-availability_classmetrics-form/meta/moodle-availability_classmetrics-form.json.js
+++
@@ -1,6 +0,0 @@
-{
-  "moodle-availability_classmetrics-form": {
-    "path": "moodle-availability_classmetrics-form.js",
-    "requires": ["base", "node", "event", "moodle-core_availability-form"]
-  }
-}
